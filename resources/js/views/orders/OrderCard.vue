90453212e6660904813e2e9b17e2ef5a89134957
Paul Brosowsky <paul.brosowsky@gmail.com>
2020-05-10 15:38:07 +0200

    Bug Orders View Filters

--- resources/js/views/orders/OrderCard.vue
+++ resources/js/views/orders/OrderCard.vue
@@ -14,10 +14,10 @@
             </div>
             <div class="flex-1 overflow-hidden">
                 <div class="flex items-center">
-                    <category-tag :category="tender.category" ></category-tag> 
+                    <category-tag :category="order.tender.category" ></category-tag> 
                     <p class="text-sm text-gray-500 leading-none pl-2" v-text="order.custom_id"></p>
                 </div>
-                <p class="text-lg truncate" v-text="tender.title"></p>
+                <p class="text-lg truncate" v-text="order.tender.title"></p>
             </div>
         </section>
 
@@ -52,23 +52,23 @@
                     <p class="uppercase text-sm text-gray-500">{{$t('tender.pickup_details')}}</p>                            
                 </div> 
                 
-                <div class="leading-tight text-gray-700" v-if="tender.pickup">
+                <div class="leading-tight text-gray-700" v-if="order.tender.pickup">
                     <p class="mb-2 leading-none">
-                        <span class="font-semibold text-black" v-text="tender.pickup.city"></span>
-                        <span class="text-sm text-gray-500" v-text="tender.pickup.country"></span>
+                        <span class="font-semibold text-black" v-text="order.tender.pickup.city"></span>
+                        <span class="text-sm text-gray-500" v-text="order.tender.pickup.country"></span>
                     </p>
                     <div class="text-sm">
                         <p>
                             <span class="text-gray-500 text-sm">{{$t('utilities.from')}}</span>
-                            {{tender.pickup.earliest_date | moment("DD.MM.YYYY")}}
+                            {{order.tender.pickup.earliest_date | moment("DD.MM.YYYY")}}
                             <span class="text-gray-500 text-sm ml-1">{{$t('utilities.to')}}</span>
-                            {{tender.pickup.latest_date | moment("DD.MM.YYYY")}}
+                            {{order.tender.pickup.latest_date | moment("DD.MM.YYYY")}}
                         </p>
                         <p>
                             <span class="text-gray-500 text-sm">{{$t('tender.loading_time')}}</span>
-                            <span v-text="tender.pickup.loading_start"></span>
+                            <span v-text="order.tender.pickup.loading_start"></span>
                             &bull;
-                            <span v-text="tender.pickup.loading_end"></span>
+                            <span v-text="order.tender.pickup.loading_end"></span>
                         </p>
                     </div>
                 </div>
@@ -78,23 +78,23 @@
                     <svg class="h-4 text-green-500 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M192 277.4h189.7l-43.6 44.7L368 352l96-96-96-96-31 29.9 44.7 44.7H192v42.8z"/><path d="M255.7 421.3c-44.1 0-85.5-17.2-116.7-48.4-31.2-31.2-48.3-72.7-48.3-116.9 0-44.1 17.2-85.7 48.3-116.9 31.2-31.2 72.6-48.4 116.7-48.4 44 0 85.3 17.1 116.5 48.2l30.3-30.3c-8.5-8.4-17.8-16.2-27.7-23.2C339.7 61 298.6 48 255.7 48 141.2 48 48 141.3 48 256s93.2 208 207.7 208c42.9 0 84-13 119-37.5 10-7 19.2-14.7 27.7-23.2l-30.2-30.2c-31.1 31.1-72.5 48.2-116.5 48.2zM448.004 256.847l-.849-.848.849-.849.848.849z"/></svg>                         
                     <p class="uppercase text-sm text-gray-500">{{$t('tender.delivery_details')}}</p>                         
                 </div> 
-                <div class="leading-tight text-gray-700" v-if="tender.delivery">
+                <div class="leading-tight text-gray-700" v-if="order.tender.delivery">
                     <p class="mb-2 leading-none">
-                        <span class="font-semibold text-black" v-text="tender.delivery.city"></span>
-                        <span class="text-sm text-gray-500" v-text="tender.delivery.country"></span>
+                        <span class="font-semibold text-black" v-text="order.tender.delivery.city"></span>
+                        <span class="text-sm text-gray-500" v-text="order.tender.delivery.country"></span>
                     </p>
                     <div class="text-sm">
                         <p>
                             <span class="text-gray-500 text-sm">{{$t('utilities.from')}}</span>
-                            {{tender.delivery.earliest_date | moment("DD.MM.YYYY")}}
+                            {{order.tender.delivery.earliest_date | moment("DD.MM.YYYY")}}
                             <span class="text-gray-500 text-sm ml-1">{{$t('utilities.to')}}</span>
-                            {{tender.delivery.latest_date | moment("DD.MM.YYYY")}}
+                            {{order.tender.delivery.latest_date | moment("DD.MM.YYYY")}}
                         </p>
                         <p>
                             <span class="text-gray-500 text-sm">{{$t('tender.loading_time')}}</span>
-                            <span v-text="tender.delivery.loading_start"></span>
+                            <span v-text="order.tender.delivery.loading_start"></span>
                             &bull;
-                            <span v-text="tender.delivery.loading_end"></span>
+                            <span v-text="order.tender.delivery.loading_end"></span>
                         </p>
                     </div>
                    
@@ -103,13 +103,13 @@
             <div class="px-5 py-2 hidden lg:block">
                 <div class="leading-tight mb-2">
                     <p class="text-gray-500 text-sm">{{$t('tender.total_weight')}} </p>
-                    <p v-if="tender.weight" v-text="tender.weight + 'kg'"></p>
+                    <p v-if="order.tender.weight" v-text="order.tender.weight + 'kg'"></p>
                 </div>
 
-                <p v-if="tender.freights">
-                    <span class="font-bold" v-text="tender.freights.length"></span>
+                <p v-if="order.tender.freights">
+                    <span class="font-bold" v-text="order.tender.freights.length"></span>
                     <span class="text-sm text-gray-500">{{
-                        tender.freights.length > 1 ? $t('tender.single_loads') : $t('tender.load')
+                        order.tender.freights.length > 1 ? $t('tender.single_loads') : $t('tender.load')
                     }}</span>
                 </p>
                 
@@ -119,7 +119,7 @@
 
         <tender-card 
             class="border-none -mx-5 -my-3 lg:hidden"  
-            :tender="tender" 
+            :tender="order.tender" 
             :order="order"
         ></tender-card>
 
@@ -131,8 +131,7 @@
 
         data(){
             return{
-                offer: this.order.offer,                
-                tender: this.order.tender,
+                offer: this.order.offer, 
                 tenderer: this.order.tenderer,
                 carrier: this.order.carrier
             }
